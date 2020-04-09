0456fd4238e22973930790e2fc12600bcc27f1cb
Matei <matei.penca1@gmail.com>
2020-04-09 13:40:06 +0200

    I added Die

--- testModule/src/testPackage/HelloWorld.java
+++ testModule/src/testPackage/HelloWorld.java
@@ -1,8 +1,11 @@
 package testPackage;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hey there\n");
         System.out.println("3+7=10\n");
+        System.out.println("DIE");
     }
 }

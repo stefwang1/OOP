8388af1f9bbc9e7dbbd6826e0123821fb8163e77
stefwang <stefwang1@gmail.com>
2020-04-09 13:09:55 +0200

    added calculation

--- testModule/src/testPackage/HelloWorld.java
+++ testModule/src/testPackage/HelloWorld.java
@@ -3,5 +3,6 @@ package testPackage;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hey there\n");
+        System.out.println("3+7=10\n");
     }
 }
